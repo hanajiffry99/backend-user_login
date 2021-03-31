84989a14186329731189c1e878618bd59433a676
Hana Jiffry <hanajiffryscg@gmail.com>
2021-03-31 17:45:32 +0530

    authentication completed

--- server.js
+++ server.js
@@ -30,10 +30,33 @@ app.use(bodyparser.json());
 app.use(bodyparser,urlencoded({extended:true}));
 
 
+//middleware that checks if JWT token exists and verifies it if it does exist.
+//In all future routes, this helps to know if the request is authenticated or not.
+app.use(function (req, res, next) {
+    // check header or url parameters or post parameters for token
+    var token = req.headers['authorization'];
+    if (!token) return next(); //if no token, continue
+   
+    token = token.replace('Bearer ', '');
+    jwt.verify(token, process.env.JWT_SECRET, function (err, user) {
+      if (err) {
+        return res.status(401).json({
+          error: true,
+          message: "Invalid user."
+        });
+      } else {
+        req.user = user; //set the user to req so other routes can use it
+        next();
+      }
+    });
+  });
+   
+
 //adding request handler
-app.get('/',(req,res) =>{
-    res.send("Welcome to our server");
-});
+app.get('/', (req, res) => {
+    if (!req.user) return res.status(401).json({ success: false, message: 'Invalid user to access it.' });
+    res.send('Welcome to the Node.js Tutorial! - ' + req.user.firstname);
+  });
 
 // validate the user credentials
 app.post('/signin', function (req, res) {
@@ -64,6 +87,38 @@ if (!user || !pwd) {
   // return the token along with user details
   return res.json({ user: userObj, token });
 });
+
+
+// verify the token and return it if it's valid
+app.get('/verifyToken', function (req, res) {
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.query.token;
+  if (!token) {
+    return res.status(400).json({
+      error: true,
+      message: "Token is required."
+    });
+  }
+  // check token that was passed by decoding token using secret
+  jwt.verify(token, process.env.JWT_SECRET, function (err, user) {
+    if (err) return res.status(401).json({
+      error: true,
+      message: "Invalid token."
+    });
+ 
+    // return 401 status if the userId does not match.
+    if (user.email !== userData.email) {
+      return res.status(401).json({
+        error: true,
+        message: "Invalid user."
+      });
+    }
+    // get basic user details
+    var userObj = utils.getCleanUser(userData);
+    return res.json({ user: userObj, token });
+  });
+});
+ 
 
 //setting up server
 app.listen(port,() =>
