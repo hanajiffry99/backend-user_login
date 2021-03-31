4927eff031d4d9cf49ae10835558ac799f0edf07
Hana Jiffry <hanajiffryscg@gmail.com>
2021-03-31 16:50:53 +0530

    enabling middleware

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 //enabling middleware to handle requests
 
+require('dotenv').config();
+
 const express = require('express');
+const cors = require('cors');
+const bodyparser=require('body-parser');
+const jwt = require('jsonwebtoken');
+const { urlencoded } = require('body-parser');
+
 const app =express();
 const port =process.env.port||4000;
+
+//enable cors
+app.use(cors());
+
+//parse application/json
+app.use(bodyparser.json());
+
+app.use(bodyparser,urlencoded({extended:true}));
+
 
 //adding request handler
 app.get('/',(req,res) =>{
